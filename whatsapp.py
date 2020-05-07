6a89e769022246a1b035c895fe7346b1b4f995a3
Kumar satyanshu <kkumarsatyanshu2107@gmail.com>
2020-05-07 20:02:30 +0530

    Add files via upload

---
+++ whatsapp.py
@@ -0,0 +1,29 @@
+# Not suitable for incognito mode or private browsing this time
+# I recommend login once to the WhatsApp web before using this script
+# This script is suitable only for Indian WhatsApp Number yet
+
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome(executable_path=r"Paste your driver path here") 
+driver.maximize_window()
+
+mobile_number = input("Enter a valid WhatsApp number : ")
+#mg = input("Write your message here : ")
+if (len(mobile_number)>10):
+  print("Invalid WhatsApp number! Kindly try again")
+else:
+  #msg = str(input("Write your message here : "))
+  driver.get('https://api.whatsapp.com/send?phone=91' + mobile_number + '&lang=en')
+  driver.find_element_by_id("action-button").click()
+  time.sleep(5)
+  driver.find_element_by_xpath("//a[contains(text(),'use WhatsApp Web')]").click()
+  time.sleep(20)
+
+  #This is the time when the user need to scan the QR code of WhatsApp web from their phone.
+
+  driver.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[2]/div/div[2]").send_keys("Paste your msg here")
+  driver.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[3]/button/span").click()
+
+  time.sleep(2)
+  driver.quit()
